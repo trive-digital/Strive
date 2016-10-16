edfb6f342f6f35a9c32cb7d220244aa877024d51
Marjan Bonus <marjan.bonus@gmail.com>
2016-10-16 13:59:54 +0200

    Change vendor name

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/JakeSharp/blank',
+    'frontend/Trive/blank',
     __DIR__
 );
